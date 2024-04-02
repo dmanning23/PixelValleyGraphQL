ef6668be4e91ae43d3508fd53ab0c96a60acbf21
Dan Manning <dmanning@oceaneering.com>
2024-04-02 15:30:32 -0500

    CORS

--- api/__init__.py
+++ api/__init__.py
@@ -1,15 +1,16 @@
-from flask import Flask
-from flask_cors import CORS
-#from keys import mongoUri
-from mongoengine import *
 import os
+from flask import Flask
+from flask_cors import CORS, cross_origin
+from mongoengine import *
 
 app = Flask(__name__)
 CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 mongoUri = os.getenv("mongoUri")
 connect(host=mongoUri, db="pixelValley") #connect for mongoengine
 
 @app.route('/')
+@cross_origin()
 def hello():
     return 'My First API !!'
