929e81b6dd9f4b84f2dd678eaf9e461b68280373
Dan Manning <dmanning@oceaneering.com>
2024-04-08 10:52:33 -0500

    deploy main to heroku

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,6 @@
 import os
 from mongoengine import *
-from keys import mongoUri
+#from keys import mongoUri
 
-#mongoUri = os.getenv("mongoUri")
-connect(host=mongoUri, db="pixelValley") #connect for mongoengine
+mongoUri = os.getenv("mongoUri")
+#connect(host=mongoUri, db="pixelValley") #connect for mongoengine
