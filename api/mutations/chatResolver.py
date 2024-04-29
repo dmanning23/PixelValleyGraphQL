564917ec701eb3fd8a209a03068e625c7e8cb52e
Dan Manning <dmanning@oceaneering.com>
2024-04-29 18:52:12 -0500

    fixed chat api

--- api/mutations/chatResolver.py
+++ api/mutations/chatResolver.py
@@ -1,19 +1,22 @@
 from api.models.conversationModel import ConversationModel
-from keys import chatApiUrl
+#from keys import chatApiUrl
 import requests
+import os
 
 def chatRequest_mutation(obj, info, scenarioId, locationId, agentIds):
     try:
-        url = f"{chatApiUrl}/chatRequest"
+        chatApiUrl = os.getenv("chatApiUrl")
+        url = f"{chatApiUrl}chatRequest"
         data = {"scenarioId": scenarioId,
                 "locationId": locationId,
                 "agentIds": agentIds }
 
-        response = requests.post(url, data=data)
+        response = requests.post(url, json=data)
 
         if response.status_code == 200:
             #get the conversation from the db
-            model = ConversationModel.objects.get(id=response["id"])
+            id = response.json()["id"]
+            model = ConversationModel.objects.get(id=id)
             payload = {
                 "success": True,
                 "conversation": model.to_dict()
