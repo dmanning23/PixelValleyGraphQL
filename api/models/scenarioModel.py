cf537e30672283df3f3cdfdc0f35f4391b404c9a
Dan Manning <dmanning@oceaneering.com>
2024-05-08 08:43:13 -0500

    format time

--- api/models/scenarioModel.py
+++ api/models/scenarioModel.py
@@ -15,7 +15,7 @@ class ScenarioModel(Document):
             "_id": self.id,
             "name": self.name,
             "description": self.description,
-            "currentDateTime": str(self.currentDateTime.strftime('%I:%M %p %d-%m-%Y')),
+            "currentDateTime": str(self.currentDateTime.strftime('%I:%M %p  %A, %B %d, %Y')),
             "seed": self.seed,
             "imageFilename": self.imageFilename,
         }
